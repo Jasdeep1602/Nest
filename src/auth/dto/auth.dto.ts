100f385387e1ccc9bd8d495324e562af99ebf82e
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2025-04-03 21:49:59 +0530

    pipes class validations

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,12 +1,11 @@
-export interface AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
-  // firstName: string;
-  // lastName: string;
-  // phoneNumber: string;
-  // address: string;
-  // city: string;
-  // state: string;
-  // country: string;
-  // zipCode: string;
 }
