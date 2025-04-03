5e61202e98021b713f06b5cdcb38382315c6911b
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2025-04-03 18:27:01 +0530

    dto and prisma connect

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
-@Controller()
+@Controller('auth')
+// @Controller('auth') // This is the base route for this controller
 export class AuthController {
-  // remove readonly
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto,
+    });
     return this.authService.signup();
   }
 
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,12 @@
+export interface AuthDto {
+  email: string;
+  password: string;
+  // firstName: string;
+  // lastName: string;
+  // phoneNumber: string;
+  // address: string;
+  // city: string;
+  // state: string;
+  // country: string;
+  // zipCode: string;
+}
